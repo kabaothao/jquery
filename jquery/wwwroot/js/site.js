722c0b459707eacba54a34545c1e40d96bfe1904
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-05-10 08:11:17 -0500

    Add project files.

---
+++ jquery/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//Every time you want to reference to jQuery you hit $
+//jQuery will always be ready in $
+//hey wait for my document to load and then we can load my codes.
+
+$(document).ready(function () {
+    $('#panel1').hide();
+
+
+});
